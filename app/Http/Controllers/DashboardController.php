221eb0c0c3de2da59d95e6c8c35cbc0339733567
NestyTampubolon <76415637+NestyTampubolon@users.noreply.github.com>
2022-06-25 23:02:27 +0700

    beranda admin diagram

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,9 +14,9 @@ class DashboardController extends Controller
     public function index()
     {
         $users = DB::table('users')->count();
-        $pemesanans = DB::table('pemesananproduk')->where('status','=',"Selesai")->count();
-        $pembookingans = DB::table('pembookinganlayanan')->where('status','=',"Diterima")->count();
-        $category[] = ['January', 'February', 'March', 'April', 'May', 'Juni', 'Juli', 'August'];
+        $pemesanans = DB::table('pemesananproduk')->where('status', '=', "Selesai")->count();
+        $pembookingans = DB::table('pembookinganlayanan')->where('status', '=', "Diterima")->count();
+        $category[] = ['January', 'February', 'March', 'April', 'May', 'June', 'Julu', 'August', 'September', 'October', 'November', 'December'];
         $totalpemesanan = PemesananProduk::select(
             DB::raw('sum(total_pembayaran) as total'),
         )->groupBy(DB::raw("DATE_FORMAT(tanggal_pemesanan,'%M')"))
@@ -27,31 +27,33 @@ class DashboardController extends Controller
 
         $bulan1 = PemesananProduk::select(
             DB::raw("DATE_FORMAT(tanggal_pemesanan,'%M') as months"),
-        )->where('status','=',"Selesai")
-        ->groupBy('months')
+        )->where('status', '=', "Selesai")
+            ->orderBy(DB::raw("Month(tanggal_pemesanan)"), 'asc')
+            ->groupBy('months')
             ->pluck('months');
 
         $data1 =  PemesananProduk::select(
             DB::raw('sum(total_pembayaran) as total'),
-        )->where('status','=',"Selesai")
-        ->groupBy(DB::raw("DATE_FORMAT(tanggal_pemesanan,'%M')"))
-        ->pluck('total');
+        )->where('status', '=', "Selesai")
+            ->groupBy(DB::raw("DATE_FORMAT(tanggal_pemesanan,'%M')"))
+            ->pluck('total');
 
 
         $bulan2 = PembookinganLayanan::select(
             DB::raw("DATE_FORMAT(tanggal_pembookingan,'%M') as months"),
-        )->where('status','=',"Diterima")
-        ->groupBy('months')
+        )->where('status', '=', "Diterima")
+            ->orderBy(DB::raw("Month(tanggal_pembookingan)"), 'asc')
+            ->groupBy('months')
             ->pluck('months');
 
         $data2 =  PembookinganLayanan::select(
             DB::raw('sum(total_pembayaran) as total'),
-        )->where('status','=',"Diterima")
-        ->groupBy(DB::raw("DATE_FORMAT(tanggal_pembookingan,'%M')"))
+        )->where('status', '=', "Diterima")
+            ->groupBy(DB::raw("DATE_FORMAT(tanggal_pembookingan,'%M')"))
             ->pluck('total');
 
 
 
-        return view('admin.dashboard', compact('users', 'pemesanans', 'pembookingans', 'bulan1', 'data1', 'bulan2', 'data2'));
+        return view('admin.dashboard', compact('users', 'pemesanans', 'pembookingans', 'bulan1', 'data1', 'bulan2', 'data2', 'category'));
     }
 }
